39502077e680ae23248a4863775f92cd4636ba96
SudikshaKolluru <93970322+SudikshaKolluru@users.noreply.github.com>
2021-11-09 12:35:57 +0530

    Create linear_nonrecurssive.c

---
+++ Week1/linear_nonrecurssive.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int linearsearch(int[],int,int,int);
+int main()
+{
+        int n, arr[10], key, result ,i;
+        printf("enter the number of elements");
+        scanf("%d", &n);
+        printf("\n enter the elements");
+        for(i=0; i<n; i++)
+        {
+                scanf("%d", &arr[i]);
+        }
+        printf("\n enter the element to find");
+        scanf("%d", &key);
+        result= linearsearch(arr,n,key,0);
+        if(result == -1)
+        {
+                printf("\n element found at %d position",result+1);
+        }
+}
+int linearsearch(int arr[10], int n, int key, int pos)
+{
+        if(pos>=n)
+                return 0;
+        else if(arr[pos]==key)
+        {
+                return pos;
+        }
+        else
+        {
+                return linearsearch(arr,n,key,pos+1);
+        }
+        return -1;
+}
